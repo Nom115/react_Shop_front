008c1c77687964e4d494951ecd4cf1fb6c6d3ecf
Nom115 <Noumair.rafiq@gmail.com>
2022-06-29 22:08:23 +0100

    Add flavicon

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,10 @@ export default function Layout({ children }) {
     return (
         <div className="content">
             <Head>
+            <title>Big Boss Fashion</title>
+            
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"></link>
+            <link rel="icon" href="../public/imgs/big_boss_nom.jpg" type="image/icon type" />
             </Head>
             <Navbar />
             { children }
