0354e68ad3bdf6c456fba010276f4d965c0315ec
Nom115 <Noumair.rafiq@gmail.com>
2022-06-30 15:05:01 +0100

    Add initial component for category

--- components/Category.js
+++ components/Category.js
@@ -1,8 +1,13 @@
-
+import Image from 'next/image'
 export default function Category(props) {
     return (
         <div className="card">
-            <img src={props.img_src} alt="picture"></img>
+            <Image 
+              src={props.img_src}
+              width="452px"
+              height="558px"
+              layout="responsive"
+              />
             <div className="card-body">
                 <h5 className="card-title">{ props.card_title }</h5>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -7,43 +7,13 @@ export default function Home() {
 
       <div className="row m-5">
         <div className="col">
-          <div className="card">
-              <Image 
-              src="/../public/imgs/male_card.jpg"
-              width="452px"
-              height="558px"
-              layout="responsive"
-              />
-              <div className="card-body">
-                  <h5 className="card-title">Mens</h5>
-              </div>
-          </div>
+          <Category img_src = "/../public/imgs/male_card.jpg" card_title="Mens"/>
         </div>
         <div className="col">
-          <div className="card">
-              <Image 
-              src="/../public/imgs/male_card.jpg"
-              width="452px"
-              height="558px"
-              layout="responsive"
-              />
-              <div className="card-body">
-                  <h5 className="card-title">Mens</h5>
-              </div>
-          </div>
+          <Category img_src = "/../public/imgs/male_card.jpg" card_title="Mens"/>
         </div>
         <div className="col">
-          <div className="card">
-              <Image 
-              src="/../public/imgs/male_card.jpg"
-              width="452px"
-              height="558px"
-              layout="responsive"
-              />
-              <div className="card-body">
-                  <h5 className="card-title">Mens</h5>
-              </div>
-          </div>
+          <Category img_src = "/../public/imgs/male_card.jpg" card_title="Mens"/>
         </div>
       </div>
     </div>
